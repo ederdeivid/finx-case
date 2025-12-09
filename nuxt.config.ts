0b7d9276a1a6c61f2a7ccf466090767d09793892
Eder Deivid <ederdeivid89@gmail.com>
2025-12-09 20:07:33 -0300

    chore: add project setup requirements and test configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/test-utils']
+  ssr: true,
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/test-utils'],
+  typescript: {
+    strict: true,
+    typeCheck: true
+  },
 })
